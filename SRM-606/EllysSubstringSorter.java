68344e27cf8acad8f4962c00b385b43128ba0ac5
Quentin Bahers <quentin.bahers@gmail.com>
2014-09-04 19:52:18 +0200

    Solve EllysSubstringSorter

---
+++ SRM-606/EllysSubstringSorter.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class EllysSubstringSorter {
+
+    public String getMin(String S, int L) {
+        String res = S;
+
+        char[] s = S.toCharArray();
+        for (int i = 0; i < s.length - L; i++) {
+            char[] tmp = s.clone();
+            Arrays.sort(tmp, i, i + L);
+            String s1 = new String(tmp);
+            if (s1.compareTo(res) < 0) res = s1;
+            
+        }
+
+        return res;
+    }
+
+}
