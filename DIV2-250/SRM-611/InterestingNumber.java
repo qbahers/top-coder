4bb653f3482f718476b36e1268475e843ac7b547
Quentin Bahers <quentin.bahers@gmail.com>
2014-12-04 12:33:00 +0100

    Fix InterestingNumber

--- DIV2-250/SRM-611/InterestingNumber.java
+++ DIV2-250/SRM-611/InterestingNumber.java
@@ -1,20 +1,25 @@
 public class InterestingNumber {
 
     public String isInteresting(String x) {
-        if (x.length() % 2 != 0) return "Not interesting";
-
-        int[] a = new int[10];
-        char[] x1 = x.toCharArray();
-        for (int i = 0; i < x1.length; i++) {
-            char c = x1[i];
+        char[] xx = x.toCharArray();
+        int N = xx.length;
+        
+        int[] freq = new int[10];
+        
+        for (int i = 0; i < N; i++) {
+            char c = xx[i];
+            
             if (c == 'X') continue;
-
+            
             int digit = Character.getNumericValue(c);
-            if (a[digit] == 1) return "Not interesting";
-            if (c != x1[i + digit + 1]) return "Not interesting";
-            x1[i + digit + 1] = 'X'; 
+            if (i + digit + 1 >= xx.length || c != xx[i + digit + 1]  || freq[digit] >= 2) 
+                return "Not interesting";
+            
+            xx[i + digit + 1] = 'X';
+            freq[digit] += 2;
         }
-        return "Interesting";
+         
+        return "Interesting";  
     }
 
 }
