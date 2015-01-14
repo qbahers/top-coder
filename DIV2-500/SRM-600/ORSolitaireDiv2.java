c96a3fe610410f37d7bc87bbe4f3a3a85124b58e
Quentin Bahers <quentin.bahers@gmail.com>
2015-01-14 22:03:43 +0100

    Solve ORSolitaireDiv2

---
+++ DIV2-500/SRM-600/ORSolitaireDiv2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class ORSolitaireDiv2 {
+
+    public int getMinimum(int[] numbers, int goal) {
+        char[] g = Integer.toBinaryString(goal).toCharArray();
+        int N = g.length;
+        
+        int[] a = new int[100];
+        outer:
+        for (int x : numbers) {
+            char[] xx = Integer.toBinaryString(x).toCharArray();
+            int M = xx.length;
+            
+            if (M > N) continue;
+            
+            for (int i = 0; i < M; i++)
+                if (xx[i] == '1' && g[N - M + i] == '0') continue outer;
+            
+            for (int i = 0; i < M; i++)
+                if (xx[i] == '1') a[M - 1 - i]++;
+        }
+        
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++)
+            if (g[i] == '1') min = Math.min(min, a[N - 1 - i]);
+        
+        return min;
+    }
+
+}
