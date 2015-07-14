cbde42d5b6f45c14208917984a98d08f1c3eb872
Quentin Bahers <quentin.bahers@gmail.com>
2015-07-14 21:58:22 +0100

    Fix indentation

--- DIV1-250/SRM-650/TaroFillingAStringDiv1.java
+++ DIV1-250/SRM-650/TaroFillingAStringDiv1.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class TaroFillingAStringDiv1 {
-	private static final long MOD = (long) (1e9 + 7);
+    private static final long MOD = (long) (1e9 + 7);
 
     public int getNumber(int N, int[] position, String value) {
         int M = position.length;
@@ -17,7 +17,7 @@ public class TaroFillingAStringDiv1 {
         long res = 1;
         for (int i = 1; i < M; i++) {
             int diff = c[i].position - c[i - 1].position;
-			if (diff % 2 == 0 && c[i].value != c[i - 1].value) {
+            if (diff % 2 == 0 && c[i].value != c[i - 1].value) {
                 res = (res * diff) % MOD;
             }
             else if (diff % 2 != 0 && c[i].value == c[i - 1].value) {
