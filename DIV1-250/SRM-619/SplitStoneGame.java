b2533a2a0347569d8396307393300cd7f183172d
Quentin Bahers <quentin.bahers@gmail.com>
2015-07-14 21:53:33 +0100

    Solve SplitStoneGame

---
+++ DIV1-250/SRM-619/SplitStoneGame.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class SplitStoneGame {
+
+	public String winOrLose(int[] number) {
+		int[][] dp = new int[51][51];
+		
+		dp[0][0] = 0;
+		
+		for (int i = 0; i <= 50; i++) {
+			for (int j = 1; j <= 50; j++) {
+				if (i - 1 >= 0) {
+					dp[i][j] = (dp[i - 1][j] + 1) % 2;
+				}
+				else if (j - 1 >= 0) {
+					dp[i][j] = (dp[i][j - 1] + 1) % 2;
+				}
+			}
+		}
+		
+		int k = 0;
+		int l = 0;
+		for (int x : number) {
+			if (x == 1) k++;
+			else        l++;
+		}
+		
+		if (k == 0 && l == 1) return "LOSE";
+		
+		return (dp[k][l] == 1) ? "WIN" : "LOSE";
+	}
+
+}
